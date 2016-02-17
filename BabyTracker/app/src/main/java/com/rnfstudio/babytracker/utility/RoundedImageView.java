3c03c4bcf83ec69f5349d9d8df652a6a7194ffc8
Roger Huang <roger00.h@gmail.com>
2016-02-18 00:47:02 +0800

    Draw rounded rectangle profile picture w/ border
    - Refer to:
    
    http://stackoverflow.com/questions/5896234/how-to-use-android-canvas-to-draw-a-roundrect-with-only-topleft-and-topright-cor
    http://blog.csdn.net/t12x3456/article/details/10432935

--- BabyTracker/app/src/main/java/com/rnfstudio/babytracker/utility/RoundedImageView.java
+++ BabyTracker/app/src/main/java/com/rnfstudio/babytracker/utility/RoundedImageView.java
@@ -5,6 +5,7 @@ import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Path;
 import android.graphics.PorterDuff;
 import android.graphics.PorterDuffXfermode;
 import android.graphics.Rect;
@@ -20,7 +21,6 @@ public class RoundedImageView extends ImageView {
 
     public RoundedImageView(Context context) {
         super(context);
-        // TODO Auto-generated constructor stub
     }
 
     public RoundedImageView(Context context, AttributeSet attrs) {
@@ -48,10 +48,8 @@ public class RoundedImageView extends ImageView {
 
         int w = getWidth(), h = getHeight();
 
-
         Bitmap roundBitmap =  getCroppedBitmap(bitmap, w);
-        canvas.drawBitmap(roundBitmap, 0,0, null);
-
+        canvas.drawBitmap(roundBitmap, 0, 0, null);
     }
 
     public static Bitmap getCroppedBitmap(Bitmap bmp, int radius) {
@@ -64,22 +62,76 @@ public class RoundedImageView extends ImageView {
                 sbmp.getHeight(), Bitmap.Config.ARGB_8888);
         Canvas canvas = new Canvas(output);
 
-        final int color = 0xffa19774;
         final Paint paint = new Paint();
-        final Rect rect = new Rect(0, 0, sbmp.getWidth(), sbmp.getHeight());
+        int bw = 10; // border width
+        final Rect innerRect = new Rect(bw, bw, sbmp.getWidth()-bw, sbmp.getHeight()-bw);
+        final Rect outerRect = new Rect(0, 0, sbmp.getWidth(), sbmp.getHeight());
 
         paint.setAntiAlias(true);
         paint.setFilterBitmap(true);
         paint.setDither(true);
         canvas.drawARGB(0, 0, 0, 0);
-        paint.setColor(Color.parseColor("#BAB399"));
-        canvas.drawCircle(sbmp.getWidth() / 2+0.7f, sbmp.getHeight() / 2+0.7f,
-                sbmp.getWidth() / 2+0.1f, paint);
-        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
-        canvas.drawBitmap(sbmp, rect, rect, paint);
 
+//        // for circle
+//        paint.setColor(Color.parseColor("#BAB399"));
+//        canvas.drawCircle(sbmp.getWidth() / 2 + 0.7f, sbmp.getHeight() / 2 + 0.7f,
+//                sbmp.getWidth() / 2 + 0.1f, paint);
+//        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+//        canvas.drawBitmap(sbmp, innerRect, innerRect, paint);
+
+        // draw image
+        paint.setColor(Color.parseColor("#BAB399"));
+        canvas.drawPath(RoundedRect(innerRect, 50.0f - bw, 50.0f - bw), paint);
+        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+        canvas.drawBitmap(sbmp, innerRect, innerRect, paint);
+
+        // draw border
+        paint.setColor(Color.WHITE);
+        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.DST_OVER));
+        canvas.drawPath(RoundedRect(outerRect, 50.0f, 50.0f), paint);
 
         return output;
     }
 
+    static public Path RoundedRect(Rect rect, float rx, float ry) {
+        return RoundedRect(rect.left, rect.top, rect.right, rect.bottom, rx, ry);
+    }
+
+    /**
+     * Tartarize's answer from:
+     *
+     * http://stackoverflow.com/questions/5896234/
+     * how-to-use-android-canvas-to-draw-a-roundrect-with-only-topleft-and-topright-cor
+     *
+     * This helps us draw rounded rectangles under API level 21
+     *
+     * For 21 and above, we can use Canvas.drawRoundRect()
+     */
+    static public Path RoundedRect(float left, float top, float right, float bottom,
+                                   float rx, float ry) {
+        Path path = new Path();
+        if (rx < 0) rx = 0;
+        if (ry < 0) ry = 0;
+        float width = right - left;
+        float height = bottom - top;
+        if (rx > width/2) rx = width/2;
+        if (ry > height/2) ry = height/2;
+        float widthMinusCorners = (width - (2 * rx));
+        float heightMinusCorners = (height - (2 * ry));
+
+        path.moveTo(right, top + ry);
+        path.rQuadTo(0, -ry, -rx, -ry);//top-right corner
+        path.rLineTo(-widthMinusCorners, 0);
+        path.rQuadTo(-rx, 0, -rx, ry); //top-left corner
+        path.rLineTo(0, heightMinusCorners);
+        path.rQuadTo(0, ry, rx, ry);//bottom-left corner
+        path.rLineTo(widthMinusCorners, 0);
+        path.rQuadTo(rx, 0, rx, -ry); //bottom-right corner
+
+        path.rLineTo(0, -heightMinusCorners);
+
+        path.close();//Given close, last lineto can be removed.
+
+        return path;
+    }
 }
